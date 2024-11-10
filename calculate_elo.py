1ead670b6b9ea3e5e54a0583ba111db737186801
RKKRRRKK <robertkresanek46@yahoo.com>
2024-11-10 20:59:33 +0100

    float to int part 2

--- calculate_elo.py
+++ calculate_elo.py
@@ -257,12 +257,12 @@ final_df = pd.concat([final_df, new_fighters_df], ignore_index=True)
 final_df.drop(columns=[col for col in final_df.columns if col.endswith('_new')], inplace=True)
 
 
-for col in ['days_peak', 'days_peak_dom', 'days_peak_dom_jj', 'name', 'peak_elo_dom_jj', 'best_win_dom_jj', 'peak_elo_dom', 'best_win_dom', 'peak_elo', 'best_win', 'nationality', 'birthplace', 'birth_date', 'association', 'weight_class', 'ufc_position', 'ufc_class', 'rn']:
+for col in ['days_peak', 'days_peak_dom', 'days_peak_dom_jj', 'name', 'peak_elo_dom_jj', 'best_win_dom_jj', 'peak_elo_dom', 'best_win_dom', 'peak_elo', 'best_win', 'nationality', 'birthplace', 'birth_date', 'association', 'weight_class', 'ufc_position', 'ufc_class']:
     final_df[col] = final_df[col].fillna(0)
 
 
 
-final_df['rn'] = final_df['rn'].fillna(0).astype(int)
+final_df['rn'] = final_df['rn'].fillna(1).astype(int)
 
 data_final = final_df.to_dict(orient='records')
 
