f5ee921129e4f1c3e5e71c6bfefd9fe8736bbc20
RKKRRRKK <robertkresanek46@yahoo.com>
2024-11-10 12:52:29 +0100

    name mismatch

--- get_fights.py
+++ get_fights.py
@@ -241,7 +241,8 @@ supabase.table('mma_fight_results').insert(data).execute()
 
 supabase.table('yet_to_come').insert([{'link': link} for link in yet_to_come]).execute()
 
-supabase.table('empty_event_pages').insert([{'link': link} for link in empty_page]).execute()
+if empty_page:  # Only proceed if empty_page is not empty
+    supabase.table('empty_event_pages').insert([{'link': link} for link in empty_page]).execute()
 
  
 print("Run Finished")
